380ccb406af237416c453d7ee7f8198188810b95
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2024-01-01 21:14:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//function to open portal 
+function africaOnline(){
+open("https://salmandascript.github.io/AfricaOnline/index.html");
+}
+
+//function to open help
+function help(){
+open("https://salmandascript.github.io/Help/Help.html");
+}
+
+//function to open contacts
+function contact(){
+open("https://salmandascript.github.io/Contact/Contact.html");
+}
+
+//function to open Terms of service 
+function termsOfService(){
+open("https://salmandascript.github.io/TermsOfService/Service.html");
+}
+
+//function to open About
+function about(){
+open("https://salmandascript.github.io/About/About.html");
+}
+
+//function to open My President programme 
+function myPresident(){
+open("https://salmandascript.github.io/MyPresident2/");
+}
+
+//function to open Capital Cities
+function capitalCities(){
+open("https://salmandascript.github.io/CapitalCities.App/");
+}
