cb2c0efc4572d4aea812d44741f4a4d57cec2bfd
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2024-01-02 12:48:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ open("https://salmandascript.github.io/AfricaOnline/index.html");
 
 //function to open help
 function help(){
-open("https://salmandascript.github.io/Help/Help.html");
+open("https://salmandascript.github.io/AboutSalmanda/help.html");
 }
 
 //function to open contacts
 function contact(){
-open("https://salmandascript.github.io/Contact/Contact.html");
+open("https://salmandascript.github.io/AboutSalmanda/contact.html");
 }
 
 //function to open Terms of service 
 function termsOfService(){
-open("https://salmandascript.github.io/TermsOfService/Service.html");
+open("https://salmandascript.github.io/AboutSalmanda/Service.html");
 }
 
 //function to open About
 function about(){
-open("https://salmandascript.github.io/About/About.html");
+open("https://salmandascript.github.io/AboutSalmanda/about.html");
 }
 
 //function to open My President programme 
 function myPresident(){
-open("https://salmandascript.github.io/MyPresident2/");
+open("https://salmandascript.github.io/MyPresident2/index.html");
 }
 
 //function to open Capital Cities
 function capitalCities(){
-open("https://salmandascript.github.io/CapitalCities.App/");
+open("https://salmandascript.github.io/CapitalCities.App/index.html");
 }
